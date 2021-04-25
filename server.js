440ac07015ae45aec3029657f71a76dba40582d6
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-04-25 11:42:21 -0500

    Server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require ("express");
+const mongoose = require ("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = expresss();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+     useUnifiedTopology: true
+});
+
+app.use(require("./routes/api.js"))
+
+app.listen(PORT, () => {
+    console.log('App running on port ${PORT}.');
+});
